8bbd7e46e3b0e976df3d198e992f621e1df3b496
JC Lenz <jc@miserablepop.com>
2019-09-09 19:16:19 -0700

    Updated js file with onkeypress function and for loop for stopping game after 10 guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,33 @@
-var guessKey = "";
+var guessLetters = ['a','b','c','d','e','f','g','h','i','j','k','l',
+'m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-$("#guessLeftCount").on("keypress", function() {
-    alert("Hey it works");
-  });
+var wins = 0;
+var losses = 0;
+var guesses = 10;
+
+var computerChoice = guessLetters[Math.floor(Math.random() * guessLetters.length)];
+
+console.log(computerChoice);
+
+document.onkeypress = function(event) {
+    var userGuess = event.key;
+
+    for (var i = 0; i < 10; i++){
+        if(userGuess === computerChoice){
+            wins++;
+        }else{
+            guesses--;
+        }
+    
+        if(guesses === 0){
+            losses++
+        }
+    
+        document.getElementById('wins').innerHTML =  wins;
+        document.getElementById('losses').innerHTML = losses;
+        document.getElementById('guesses').innerHTML = guesses;
+        document.getElementById('userChoice').insertAdjacentHTML("beforeend", userGuess + ", ");
+    }
+    
+}   
+
