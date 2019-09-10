9e4fdaa0376b9b0e3888aeee0b098fbd0804c09d
JC Lenz <jc@miserablepop.com>
2019-09-10 00:12:33 -0700

    Updated game.js with new functions to handle the game resets

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,63 @@
-var guessLetters = ['a','b','c','d','e','f','g','h','i','j','k','l',
-'m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+// VARIABLES
+// ==========================================================================
+
 
 var wins = 0;
 var losses = 0;
-var guesses = 10;
+var guesses = 9;
 
-var computerChoice = guessLetters[Math.floor(Math.random() * guessLetters.length)];
+var guessLetters = ['a','b','c','d','e','f','g','h','i','j','k','l',
+'m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
+function randomLetter(){
+    var random_choice = guessLetters[Math.floor(Math.random() * guessLetters.length)];
+    return random_choice;
+}
+
+var computerChoice = randomLetter();
 console.log(computerChoice);
 
+
 document.onkeypress = function(event) {
+
+    function resetWin(){
+        wins++;
+        document.getElementById("wins").innerHTML =  wins;
+        document.querySelector("#userChoice").innerHTML = " ";
+        computerChoice = randomLetter();
+        console.log(computerChoice);
+    }
+    
+    function wrongGuess(){
+        guesses--;
+        document.querySelector("#userChoice").insertAdjacentHTML("beforeend", userGuess + ", ");
+        document.getElementById("guesses").innerHTML = guesses;
+    }
+    
+    function resetLoss(){
+        guesses = 9;
+        losses++;
+        document.querySelector("#guesses").innerHTML = guesses;
+        document.querySelector("#userChoice").innerHTML = " ";
+        document.getElementById("losses").innerHTML = losses;
+        computerChoice = randomLetter();
+        console.log(computerChoice);
+    }
+
     var userGuess = event.key;
 
-    for (var i = 0; i < 10; i++){
-        if(userGuess === computerChoice){
-            wins++;
-        }else{
-            guesses--;
-        }
-    
-        if(guesses === 0){
-            losses++
-        }
-    
-        document.getElementById('wins').innerHTML =  wins;
-        document.getElementById('losses').innerHTML = losses;
-        document.getElementById('guesses').innerHTML = guesses;
-        document.getElementById('userChoice').insertAdjacentHTML("beforeend", userGuess + ", ");
+    if(userGuess === computerChoice){
+        resetWin();
+        
+    }else{
+        wrongGuess();
     }
+
+    if(guesses === 0){
+        resetLoss();
+    }
+
+    
     
 }   
 
